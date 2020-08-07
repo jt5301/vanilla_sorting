ba9d9e409ca666128f740ddb9c9a6cfb6c176808
John Tieu <johnson.tieu@gmail.com>
2020-08-07 18:52:59 -0400

    starting frame work

--- app.js
+++ app.js
@@ -39,7 +39,41 @@ const quickSort = document.getElementById('quick')
 quickSort.addEventListener('click', () => {
   const frames = []
   quickSortFunction(heights, frames)
-  console.log(heights)
+  console.log(frames)
+  for (let i = 0; i < frames.length; i++) {
+    switch (frames[i][frames[i].length - 1]) {
+      case 'start':
+        setTimeout(() => {
+          console.log('start')
+          const bar = document.getElementById(`${frames[i][0]}`)
+          bar.style.backgroundColor = 'cornflowerblue'
+        }, 1000 * i)
+        break
+      case 'color1':
+        setTimeout(() => {
+          console.log('color1')
+          const bar = document.getElementById(`${frames[i][1]}`)
+          bar.style.backgroundColor = 'cornflowerblue'
+        }, 1000 * i)
+        break
+      case 'height1':
+        let temp = ""
+        setTimeout(() => {
+          console.log(frames[i])
+          const bar1 = document.getElementById(`${frames[i][0]}`)
+          const bar2 = document.getElementById(`${frames[i][1]}`)
+          temp = bar1.style.height
+          bar1.style.height = bar2.style.height
+        }, 1000 * i)
+        setTimeout(() => {
+          const bar = document.getElementById(`${frames[i][1]}`)
+          bar.style.height = temp
+        }, 1000 * i)
+        break
+      case 'height2':
+        break
+    }
+  }
 })
 
 const mergeSort = document.getElementById('merge')
--- sortApps/quickSort.js
+++ sortApps/quickSort.js
@@ -4,6 +4,7 @@ export function quickSortFunction(heights, frames) {
 }
 
 function quickSort(array, frames, left = 0, right = array.length - 1) {
+  console.log('called')
   if (left < right) {
     let pivotIndex = pivot(array, frames, left, right)
     console.log(pivotIndex)
@@ -16,16 +17,22 @@ function quickSort(array, frames, left = 0, right = array.length - 1) {
 function pivot(array, frames, start = 0, end = array.length - 1) {
   let pivot = array[start]
   let swapIndex = start
-  frames.push([swapIndex], 'start')
+  frames.push([swapIndex, 'start'],)
   for (let i = start + 1; i <= end; i++) {
+    frames.push([swapIndex, i, 'color1'])
     if (pivot > array[i]) {
       swapIndex += 1
-      frames.push([i, swapIndex], swap)
+      frames.push([swapIndex, i, 'height1'])
+      frames.push([swapIndex, i, 'height2'])
       swap(swapIndex, i, array)
+    }
+    else {
+      frames.push([swapIndex, i, 'noSwap'])
     }
   }
   swap(start, swapIndex, array)
-  frames.push([[k]])
+  frames.push([start, swapIndex, 'height1'])
+  frames.push([start, swapIndex, 'height2'])
   return swapIndex
 }
 
